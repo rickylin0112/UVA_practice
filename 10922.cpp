616bcb7207d171dce6e6f80e91c6bb90831def02
rickylin0112 <ricky2256@gmail.com>
2021-09-05 00:06:44 +0800

    the third update

--- 10922.cpp
+++ 10922.cpp
@@ -1,26 +1,34 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-int main(){
-	int N;
-	char T[100];
-	cin >> T ;
-	int sum = 0;
-	int degree = 0;
-	for (int i = 0; i < T.size(); ++i)
-	{
-		sum += T[i];
-		degree ++;
-	}
-	if (sum % 10 > 0){
-		sum = sum %10 + (sum - (sum%10));
-		degree ++ ; 
-	}
-	else if (sum % 10 < 0 && sum %9 == 0) {
-		printf("%d is a multiple of 9 and has 9-degree %d",T,degree);
 
+int main()
+{
+	string a;
+	while (cin >> a)
+	{
+		if (a == "0")
+		{
+			break;
+		}
+		else 
+		{
+			string sum = 0;
+			int tmp = 0;
+
+
+			for (int i = 0; i < a.size(); ++i)
+			{
+				sum += a[i]-'0';
+			}
+			if ((sum - '0')%10 > 1 ){
+				back
+			}
+		}
+			
 	}
+
 	return 0;
-	}
+}
 
 
