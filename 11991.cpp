2adb19720f0b692b6120bd0cdd201fad2cde2cde
rickylin0112 <ricky2256@gmail.com>
2021-09-05 23:42:23 +0800

    我覺得我現在越打越奇怪惹

---
+++ 11991.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n ,m;
+	string arr;
+	string com;
+	cin >> n >> m;
+	for (int i = 0; i < n; ++i)
+	{
+		cin >> arr[i];
+	}
+	for (int i = 0; i < arr.size(); ++i)
+	{
+		arr = arr[i] - '0';
+	}
+	while (m)
+	{
+		int count = 0;
+		int tmp = 0;
+		int math = 0;
+		int cor = 0;
+		for (int i = 0; i < 1; ++i)
+		{
+			cin >> com[i];
+		}
+		tmp = com[1] - '0';
+		math = com[2] - '0';
+		for (int i = 0; i < n; ++i)
+		{
+			if (math == (arr[i]-'0'))
+			{
+				count++;
+				cor = i;
+				if (count == tmp)
+				{
+					break;
+				}
+			}
+		}
+	}
+}
