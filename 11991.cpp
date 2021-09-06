649a44e5feefa7684ae62d091f4d506f4067788a
rickylin0112 <ricky2256@gmail.com>
2021-09-06 08:35:07 +0800

    我又死掉了可是我覺得就差那麼一點點

--- 11991.cpp
+++ 11991.cpp
@@ -4,45 +4,88 @@ using namespace std;
 int main()
 {
 	int n ,m;
-	string arr;
-	string com;
 	cin >> n >> m;
+	string arr;
 	for (int i = 0; i < n; ++i)
 	{
 		cin >> arr[i];
 	}
-	for (int i = 0; i < arr.size(); ++i)
+	for (int i = 0; i < m; ++i)
 	{
-		arr = arr[i] - '0';
-	}
-	while (m)
-	{
-		int count = 0;
-		int tmp = 0;
-		int math = 0;
-		int cor = 0;
-		for (int i = 0; i < 1; ++i)
-		{
-			cin >> com[i];
-		}
-		tmp = com[1] - '0';
-		math = com[2] - '0';
-		for (int i = 0; i < n; ++i)
-		{
-			if (math == (arr[i]-'0'))
+		int a, b;
+		while (cin >> a >>b)
 			{
-				count++;
-				cor = i;
-				if (count == tmp)
+				int tmp = 0 ;
+				while (n)
 				{
-					break;
+
+					if (b != (int)arr[i]-'0'){
+						tmp ++;
+					}
+					else if(b == (int)arr[i]- '0')
+					{
+						tmp ++;
+						if  (b = tmp)
+						{
+							break;
+						}
+						else 
+						{
+							tmp ++;
+						}
+					}
+					cout << tmp << "\n";
 				}
-				else {
-					cout << "0\n";
-				}
+				
 			}
-			cout << cor << "\n";
-		}
 
 	}
 }
+
+
+
+
+
+
+// int n ,m;
+// 	string arr;
+// 	string com;
+// 	cin >> n >> m;
+// 	for (int i = 0; i < n; ++i)
+// 	{
+// 		cin >> arr[i];
+// 	}
+// 	for (int i = 0; i < arr.size(); ++i)
+// 	{
+// 		arr = arr[i] - '0';
+// 	}
+// 	while (m)
+// 	{
+// 		int count = 0;
+// 		int tmp = 0;
+// 		int math = 0;
+// 		int cor = 0;
+// 		for (int i = 0; i < 1; ++i)
+// 		{
+// 			cin >> com[i];
+// 		}
+// 		tmp = com[1] - '0';
+// 		math = com[2] - '0';
+// 		for (int i = 0; i < n; ++i)
+// 		{
+// 			if (math == (arr[i]-'0'))
+// 			{
+// 				count++;
+// 				cor = i;
+// 				if (count == tmp)
+// 				{
+// 					break;
+// 				}
+// 				else {
+// 					cout << "0\n";
+// 				}
+// 			}
+// 			cout << cor << "\n";
+// 		}
+
+// 	}
